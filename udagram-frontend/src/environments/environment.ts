fd0efac1bff9f87dd6f65f42b5641b8d4f28b0f1
Huynh Truong <chuongduong29@gmail.com>
2022-07-16 12:31:38 +0700

    TruongH: Update file environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af3df4626cedf4bc683ae518b8dd0b33-1127357689.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
