db9e7131e042839b65e7027eed22e768cae8e3c0
Huynh Truong <chuongduong29@gmail.com>
2022-07-17 16:49:44 +0700

    TruongH: Update files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: 'http://aac21847ff2324cfe857c1934ea11cc6-584263321.us-west-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://aac21847ff2324cfe857c1934ea11cc6-584263321.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
